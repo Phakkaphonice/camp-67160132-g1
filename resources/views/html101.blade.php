6af01055854fa2f4067a3c6291eb5532d54f8aee
Apinya <67160021@go.buu.ac.th>
2025-12-04 18:59:32 +0700

    week2

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,53 @@
+<!-- file : resources/views/html101.blade.php -->
+ <!DOCTYPE html>
+ <html>
+    <head>
+        <title>ส่วนหัว HTML </title>
+        <link rel="stylesheet" href="ssc/bootstrap.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: "Sarabun", sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <h1>Workshop #HTML - FORM</h1>
+            <form>
+                <div class="row">
+                    <div class="col-sm-12 col-md-4">
+                        <label for="fname">ชื่อ</label>
+                    </div>
+                    <div class="col">
+                        <input id="fname" class="form-control">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <style>
+            h1 {color: red;}
+        </style>
+        <h1 id="" class="">&lt;HTML 101</h1>
+        <h2 style = "color : blue;"><br><br> HTML 101</h2>
+        <h3>HTML 101</h3>
+        <hr />
+        <hr>
+        <table border="1" width="100%"> 
+            <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                    <td>ชื่อ</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>phakkaphon</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
