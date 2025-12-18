5f90410550d5ffbc6d3ebeb972b54e83f5bbfa73
Phakkaphonice <icepigmoom@gmail.com>
2025-12-19 00:49:04 +0700

    Homework_week2

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -1,7 +1,7 @@
 @extends('template.default')
 @section('content')
 
-<h2 class="row mt-5 col-sm-12 " >Workshop #HTML - FORM</h2>
+<h2 class="row mt-5 col-sm-12 " >Workshop #HTML - FORM </h2>
 
             <form>
                 <div class="row mt-5">
@@ -9,7 +9,7 @@
                         <label for="fname">ชื่อ</label>
                     </div>
                     <div class="col">
-                        <input id="fname" class="form-control">
+                        <input id="fname" class="form-control ">
                         <div class="valid-feedback">
                             ถูกต้อง
                         </div>
@@ -18,134 +18,195 @@
                         </div>
                     </div>
                 </div>
-            </form>
 
-            <form >
+         
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
-                        <label for="fname2">นามสกุล</label>
+                        <label for="lname">นามสกุล</label>
                     </div>
                     <div class="col">
-                        <input id="fname2" class="form-control" >
+                        <input id="lname" class="form-control" >
+                        <div class="valid-feedback">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback">
+                            โปรดระบุนามสกุล
+                        </div>
                     </div>
                 </div>
-            </form>
+           
 
-            <form>
+           
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
-                        <label for="date">วัน/เดือน/ปีเกิด</label>
+                        <label for="day">วัน/เดือน/ปีเกิด</label>
                     </div>
                     <div class="col">
-                        <input type="date" class="form-control">
+                        <input id= "day" type="date" class="form-control">
+                        <div class="valid-feedback">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback">
+                            โปรดระบุ วัน/เดือน/ปีเกิด
+                        </div>
                     </div>
                 </div>
-            </form>
+            
 
-            <form >
+         
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
                         <label for="age">อายุ</label>
                     </div>
                     <div class="col">
                         <input id="age" class="form-control" >
+                        <div class="valid-feedback">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback">
+                            โปรดระบุอายุ
+                        </div>
                     </div>
                 </div>
-            </form>
+            
 
-            <form>
+         
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
                         <label for="date">เพศ</label>
                     </div>
                     <div class="col">
-                        <input type="radio" id="man" name="sex" value="Man">
-                        <label for="sex">ชาย</label>
+                        <div class="d-flex flex-wrap gap-3">
+                        <div class="form-check form-check-inline">
+                        <input type="radio" id="man" name="sex" value="Man" class="form-check-input">
+                        <label class="form-check-lable" for="man">ชาย</label>
+                        </div>
 
-                        <input type="radio" id="woman" name="sex" value="Woman">
-                        <label for="sex">หญิง</label>
+                        <div class="form-check form-check-inline">
+                        <input type="radio" id="woman" name="sex" value="Woman" class="form-check-input" >
+                        <label class="form-check-lable" for="woman">หญิง</label>
+                        </div>
 
-                        <input type="radio" id="lgbt" name="sex" value="LGBT">
-                        <label for="sex">ไม่ระบุ</label>
+                        <div class="form-check form-check-inline">
+                        <input type="radio" id="lgbt" name="sex" value="LGBT" class="form-check-input" >
+                        <label class="form-check-lable" for="lgbt">ไม่ระบุ</label>
+                        </div>
+                    </div>
+
+                        <div class="valid-feedback " id="sex-success">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback" id="sex-error">
+                            โปรดเลือกเพศ
+                        </div>
                     </div>
                 </div>
-            </form>
 
-            <form>
+
+          
                 <div class="row mt-3">
-                    <div class="col-sm-12 col-md-4">
+                    <div class="col-sm-12 col-md-4 ">
                         <label for="photo">รูป</label>
                     </div>
-                    <div class="col">
-                        <input type="file" id="myfile" name="myfile">
+                    <div class="col ">
+                        <input type="file" id="myfile" name="myfile" class="form-control">
+                        <div class="valid-feedback">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback">
+                            โปรดเลือกรูป
+                        </div>
                     </div>
                 </div>
-            </form>
 
-            <form >
+
+            
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
                         <label for="Map">ที่อยู่</label>
                     </div>
                     <div class="col">
-                        <textarea id="w3review" name="w3review" rows="4" cols="95" class="form-control" ></textarea>
+                        <textarea id="home" name="w3review" rows="4" cols="95" class="form-control" ></textarea>
+                        <div class="valid-feedback">
+                            ถูกต้อง
+                        </div>
+                        <div class="invalid-feedback">
+                            โปรดระบุที่อยู่
+                        </div>
                     </div>
                 </div>
-            </form>
+          
 
-            <form >
+           
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
-                        <label for="color">สีที่ชอบ</label>
+                        <label for="favoritecolor">สีที่ชอบ</label>
                     </div>
                     <div class="col">
-                        <select class="form-select" cla>
+                        <select id='favoritecolor'class="form-select col-sm-12 col-md- form-control">
+                        <option value="">เลือกสีที่ชอบ</option> 
                         <option value="red">เเดง</option>
                         <option value="blue">น้ำเงิน</option>
                         <option value="black">ดำ</option>
                         <option value="while">ขาว</option>
                         </select>
+                        <div class="invalid-feedback">โปรดระบุสีที่ชอบ</div>
                     </div>
                 </div>
-            </form>
+            
 
-             <form >
+             
                 <div class="row mt-3">
                     <div class="col-sm-12 col-md-4">
-                        <label for="song">เเนวเพลงที่ชอบ</label>
+                        <label>แนวเพลงที่ชอบ</label>
                     </div>
                     <div class="col">
-                        <input type="radio" id="mysong1" name="music" value="1">
-                        <label for="sex">เพื่อชีวิต</label>
+                        <div class="d-flex flex-wrap gap-3">
+                             <div class="form-check form-check-inline">   
+                            <input type="radio" id="mysong1" name="music" value="1" class="form-check-input">
+                            <label class="form-check-label" for="mysong1">เพื่อชีวิต</label>
+                        </div>
 
-                        <input type="radio" id="mysong2" name="music" value="1">
-                        <label for="sex">ลูกทุ่ง</label>
+                        <div class="form-check form-check-inline">  
+                            <input type="radio" id="mysong2" name="music" value="2" class="form-check-input">
+                            <label class="form-check-label" for="mysong2">ลูกทุ่ง</label>
+                        </div>
 
-                        <input type="radio" id="mysong3" name="music" value="1">
-                        <label for="sex">อื่นๆ</label>
+                        <div class="form-check form-check-inline">
+                            <input type="radio" id="mysong3" name="music" value="3" class="form-check-input">
+                            <label class="form-check-label" for="mysong3">ฮิปฮอป</label>
+                        </div>
+                        </div>
+
+                        <div class="valid-feedback" id="music-success">ถูกต้อง</div>
+                        <div class="invalid-feedback" id="music-error">โปรดเลือกแนวเพลง</div>
                     </div>
                 </div>
-            </form>
-
-            <form>
-             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-             <label for="vehicle1">ยินยอมให้เก็บข้อมูล</label><br>
-            </form>
-
-
-            <form>
+            
+                 <div class="row mt-3">
+                     <div class="col-sm-12 col-md-4"><div class="form-check">
+                        <input id="consent_check" type="checkbox" class="form-check-input">
+                        <label for="consent_check"> ยินยอมให้เก็บข้อมูล </label>
+                        <div class="valid-feedback">ขอบคุณ</div>
+                        <div class="invalid-feedback">โปรดติ๊กยินยอมเพื่อดำเนินการต่อ</div>
+                        </div>
+                    </div>
+                </div>
+             
+             
+    
                  <div class="row mt-3" >
                     <div class="col-sm-12 col-md-4">
                         <button class="btn btn-light" type="reset">Reset</button>
                     </div>
                     <div class="col">
-                        <button class="btn btn-success" type="button">Submit</button>
+                        <button class="btn btn-success" onclick="clickMe()" type="button">Submit</button>
                     </div>
                 </div>
             </form>
 
         <style>
-            h1 {color: red;}
+            h1 {color: while;}
         </style>
         <style>
             h2 {color: gray;}
@@ -155,10 +216,28 @@
 
 @push('scripts')
     <script>
+    
         let clickMe = function(){
-           let fname = document.getElementById('fname')
-        //    fname.value = "from ClickMe"
-        //    console.log(fname.value)
+            let fname = document.getElementById('fname')
+            let lname = document.getElementById('lname')
+            let day = document.getElementById('day')
+            let age = document.getElementById('age')
+            let home = document.getElementById('home')
+            let sexInputs = document.getElementsByName('sex');
+            let isSexChecked = document.querySelector('input[name="sex"]:checked');
+            let sexError = document.getElementById('sex-error');
+            let sexSuccess = document.getElementById('sex-success');
+            let myfile = document.getElementById('myfile');
+            let favoritecolor = document.getElementById('favoritecolor');
+            let musicInputs = document.getElementsByName('music');
+            let isMusicChecked = document.querySelector('input[name="music"]:checked');
+            let musicError = document.getElementById('music-error');
+            let musicSuccess = document.getElementById('music-success');
+            let consent = document.getElementById('consent_check');
+            
+            
+
+        
 
           if(fname.value == ""){
             fname.classList.remove('is-valid')
@@ -167,8 +246,100 @@
             fname.classList.remove('is-invalid')
             fname.classList.add('is-valid')
           }
+
+        //นามสกุล
+          if(lname.value == ""){
+            lname.classList.remove('is-valid')
+            lname.classList.add('is-invalid')
+          } else{
+            lname.classList.remove('is-invalid')
+            lname.classList.add('is-valid')
+          }
+
+        //วันเกิด
+          if(day.value == ""){
+            day.classList.remove('is-valid')
+            day.classList.add('is-invalid')
+          } else{
+            day.classList.remove('is-invalid')
+            day.classList.add('is-valid')
         }
 
+        //อายุ
+          if(age.value == ""){
+            age.classList.remove('is-valid')
+            age.classList.add('is-invalid')
+          } else{
+            age.classList.remove('is-invalid')
+            age.classList.add('is-valid')
+        }
+
+        //ที่อยู่
+          if(home.value == ""){
+            home.classList.remove('is-valid')
+            home.classList.add('is-invalid')
+          } else{
+            home.classList.remove('is-invalid')
+            home.classList.add('is-valid')
+        }
+
+        //เพศ
+       if (isSexChecked == null) {
+        sexInputs.forEach(input => input.classList.add('is-invalid'));
+        sexError.style.setProperty('display', 'block', 'important');
+        sexSuccess.style.setProperty('display', 'none', 'important');
+        } else {
+        sexInputs.forEach(input => {
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+            });
+        sexError.style.setProperty('display', 'none', 'important');
+        sexSuccess.style.setProperty('display', 'block', 'important');
+        }
+
+        //รูป
+        if (myfile.value == "") {
+            myfile.classList.remove('is-valid');
+            myfile.classList.add('is-invalid');
+        } else {
+            myfile.classList.remove('is-invalid');
+            myfile.classList.add('is-valid');
+        }
+        
+        //สี
+        if (favoritecolor.value == "") {
+            favoritecolor.classList.remove('is-valid');
+            favoritecolor.classList.add('is-invalid');
+        } else {
+            favoritecolor.classList.remove('is-invalid');
+            favoritecolor.classList.add('is-valid');
+        }
+
+    
+        // เพลง
+        if (isMusicChecked == null) {
+            musicError.style.setProperty('display', 'block', 'important');
+            musicSuccess.style.setProperty('display', 'none', 'important');
+        } else {
+            musicError.style.setProperty('display', 'none', 'important');
+            musicSuccess.style.setProperty('display', 'block', 'important');
+        }
+
+        //เช็ค
+        if (!consent.checked) {
+            consent.classList.remove('is-valid');
+            consent.classList.add('is-invalid');
+        } else {
+            consent.classList.remove('is-invalid');
+            consent.classList.add('is-valid');
+        }
+
+        
+        
+
+        
+
+    
         let myfunc = (callback)=>{
             callback("in callback")
         }
@@ -176,16 +347,9 @@
         callMe = (param)=>{
             console.log(param)
         }
-
-        myfunc(callMe)
-
+    }
 
 
-       let myvar1 = 1
-       let myvar2 = "1"
-       myvar2 = parseInt(myvar2)
 
-       console.log(myvar2+myvar1+"\n\n\n\nทดสอบ")
-       console.log(1=="1")
     </script>
 @endpush
